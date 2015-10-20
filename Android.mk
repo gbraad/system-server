4c6acd737f5a09c1274ec2daa2ccaf9eca0c9026
Gerard Braad — 吉拉德 <me@gbraad.nl>
2015-10-20 07:52:09 +0000

    Device paths

--- Android.mk
+++ Android.mk
@@ -5,14 +5,14 @@ SYSD_PATH := $(LOCAL_PATH)
 .phony: sysd
 sysd:
 	cd $(SYSD_PATH); npm install --production; cd $(ANDROID_BUILD_TOP)
-	mkdir -p $(TARGET_OUT)/usr/lib/node_modules/sysd
-	rm -rf $(TARGET_OUT)/usr/lib/node_modules/sysd/*
-	cp -rf $(SYSD_PATH)/bin $(TARGET_OUT)/usr/lib/node_modules/sysd/
-	cp -rf $(SYSD_PATH)/lib $(TARGET_OUT)/usr/lib/node_modules/sysd/
-	cp -rf $(SYSD_PATH)/node_modules $(TARGET_OUT)/usr/lib/node_modules/sysd/
+	mkdir -p $(TARGET_OUT)/system/lib/node_modules/sysd
+	rm -rf $(TARGET_OUT)/system/lib/node_modules/sysd/*
+	cp -rf $(SYSD_PATH)/bin $(TARGET_OUT)/system/lib/node_modules/sysd/
+	cp -rf $(SYSD_PATH)/lib $(TARGET_OUT)/system/lib/node_modules/sysd/
+	cp -rf $(SYSD_PATH)/node_modules $(TARGET_OUT)/system/lib/node_modules/sysd/
 	mkdir -p $(TARGET_OUT)/bin
 	rm -rf $(TARGET_OUT)/bin/sysd
-	ln -sf ../usr/lib/node_modules/sysd/bin/sysd $(TARGET_OUT)/bin/sysd
+	ln -sf ../system/lib/node_modules/sysd/bin/sysd $(TARGET_OUT)/bin/sysd
 	chmod a+x $(TARGET_OUT)/bin/sysd
 
 ALL_MODULES += sysd
